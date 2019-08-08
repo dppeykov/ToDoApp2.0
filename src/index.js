edfffcc21ee9ca6e5f882f6992bc7e9154b4767e
Damyan Peykov <dppeykov@gmail.com>
2019-08-08 08:57:18 +0200

    Updating the base with bootstrap and some module movements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
